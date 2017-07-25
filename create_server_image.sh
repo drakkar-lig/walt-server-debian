72925ad8cb0b0a1824f892680850a1f95e8b29c9
Etienne Dublé <etienne.duble@imag.fr>
2017-07-25 13:42:53 +0200

    Use tftpd-hpa instead of tftpd (much faster transfers)

--- create_server_image.sh
+++ create_server_image.sh
@@ -33,7 +33,7 @@ PACKAGES=$(echo python-pip binfmt-support qemu-user-static \
 		snmp-mibs-downloader snimpy isc-dhcp-server \
 		nfs-kernel-server ntpdate ntp lockfile-progs \
 		uuid-runtime postgresql python-psycopg2 \
-        firmware-linux-nonfree ptpd tftpd)
+        firmware-linux-nonfree ptpd tftpd-hpa)
 
 APT_GET_INSTALL="DEBIAN_FRONTEND=noninteractive \
 		apt-get install -y --no-install-recommends"
